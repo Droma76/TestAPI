010e2f53d2dae5629ddd7abf52751bd72673fd29
Hadders <droma.co.uk@gmail.com>
2018-12-18 22:12:28 +0000

    added TokenController

--- TestAPI/Controllers/TokenController.cs
+++ TestAPI/Controllers/TokenController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,5 +13,21 @@ namespace TestAPI.Controllers
     [ApiController]
     public class TokenController : ControllerBase
     {
+        static string GetToken(string url, string userName, string password)
+        {
+            var pairs = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>( "grant_type", "password" ),
+                        new KeyValuePair<string, string>( "username", userName ),
+                        new KeyValuePair<string, string> ( "Password", password )
+                    };
+            var content = new FormUrlEncodedContent(pairs);
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            using (var client = new HttpClient())
+            {
+                var response = client.PostAsync(url + "Token", content).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
     }
 }
